b6d451bbaf5f8a2fc5a22390ee91272be3bc7ed4
zahiruldu <zahirul.arb@gmail.com>
2017-04-23 17:56:40 +0600

    image effects

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -68,16 +68,60 @@ export class LoginComponent implements OnInit {
 
   }
 
+  
+
   changeColor() {
-   Caman("#canvas-id", function() {
-          this.brightness(20);
-          this.render(function(){
-            this.save('hello.png');
-            console.log(this)
+   let c = Caman("#canvas", function() {
+          this.brightness(10);
+          this.contrast(30);
+          this.sepia(60);
+          this.saturation(-30);
+          this.render(function() {
+            //console.log(this.toBase64());
+            this.save('assets/hello.jpg');
           });
+
         });
+
+   console.log(c.renderer.c);
   
     alert('applied')
+  }
+
+  drawImage() {
+    var canvas: HTMLCanvasElement;
+    var ctx: CanvasRenderingContext2D;
+     canvas = <HTMLCanvasElement>document.getElementById('canvas');
+     ctx = canvas.getContext("2d");
+
+     let img2 = new Image();
+     img2.crossOrigin = '';
+     img2.src = "assets/test.jpg";
+
+     //console.log(hello);
+
+     // var img: HTMLImageElement;
+     //  var x: number = 50;
+     //  var y: number = 50;
+     //  img = <HTMLImageElement>document.getElementById('spaceship');
+     //  img.src = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+
+   // let canvas = document.getElementById('canvas');
+   //  let ctx = canvas.getContext('2d');
+
+   //  /* Enable Cross Origin Image Editing */
+   // let img = new Image();
+   //  img.crossOrigin = '';
+   //  img.src = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+
+    // img.onload = function() {
+    //   canvas.width = img.width;
+    //   canvas.height = img.height;
+    //   ctx.drawImage(img, 0, 0, img.width, img.height);
+    // }
+
+    ctx.drawImage(img2, 0, 0, 500, 500);
+
   }
 
   // Caman("#canvas-id", function() {});
